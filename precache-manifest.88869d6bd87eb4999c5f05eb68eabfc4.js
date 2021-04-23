e47e19eb1b22268d4e3cc253568415a6bfaa7a69
Gaborelu <gabor28catalin@gmail.com>
2021-04-23 23:37:59 +0300

    Updates

---
+++ precache-manifest.88869d6bd87eb4999c5f05eb68eabfc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835ef6f0a535e52183f774019fa4fbb9",
+    "url": "/Budget-App/index.html"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/Budget-App/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/Budget-App/static/js/2.360a4eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-App/static/js/2.360a4eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab781585a57054280e12",
+    "url": "/Budget-App/static/js/main.8840b9b3.chunk.js"
+  },
+  {
+    "revision": "c5908768894877b9a47f",
+    "url": "/Budget-App/static/js/runtime-main.d9246cfc.js"
+  }
+]);
